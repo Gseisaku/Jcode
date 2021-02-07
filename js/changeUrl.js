f7bfcbb5f6f0d5b907db4640237ed20070fa6120
WATANABE <watanabe@grounds.jp>
2021-02-07 10:20:34 +0900

    特定のURLの書き換え

---
+++ js/changeUrl.js
@@ -0,0 +1,6 @@
+$(window).on('load', function() {
+    var href = location.href;
+    if(href == "https://www.oideyasukyoto2.com/kgp_notice.html"){
+        history.replaceState('','','https://www.oideyasukyoto2.com/');
+    }
+});
